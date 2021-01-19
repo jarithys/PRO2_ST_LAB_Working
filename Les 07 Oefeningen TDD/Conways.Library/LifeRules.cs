315f47271e33543f0871bdd4da759e5bf24545ab
jarithys <s115309@ap.be>
2021-01-19 22:08:28 +0100

    Les 7 - Conways Game of Life

--- Les 07 Oefeningen TDD/Conways.Library/LifeRules.cs
+++ Les 07 Oefeningen TDD/Conways.Library/LifeRules.cs
@@ -2,7 +2,28 @@
 
 namespace Conway.Library
 {
+    public enum CellState
+    {
+        Alive,
+        Dead
+    }
+
     public class LifeRules
     {
+        public static CellState GetNewState(CellState currentState, int liveNeighbors)
+        {
+            switch (currentState)
+            {
+                case CellState.Alive:
+                    if (liveNeighbors < 2 || liveNeighbors > 3)
+                        return CellState.Dead;
+                    break;
+                case CellState.Dead:
+                    if (liveNeighbors == 3)
+                        return CellState.Alive;
+                    break;
+            }
+            return currentState;
+        }
     }
 }
