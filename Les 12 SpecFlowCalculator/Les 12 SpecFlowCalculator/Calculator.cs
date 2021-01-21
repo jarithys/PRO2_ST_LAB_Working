8b62df7fad4e625e57c90c13be6948df15101d5f
jarithys <s115309@ap.be>
2021-01-21 11:20:30 +0100

    Les 12 - SpecFlowCalculator

---
+++ Les 12 SpecFlowCalculator/Les 12 SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Les_12_SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+
+    }
+}
